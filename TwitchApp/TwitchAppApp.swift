d83c984c246b3f95b10ac26a3f577678771a14ab
Vadim Shinkarenko <v.shinkarenko@notamedia.ru>
2023-06-06 15:41:34 +0300

    Initial Commit

---
+++ TwitchApp/TwitchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitchAppApp.swift
+//  TwitchApp
+//
+//  Created by Vadim Shinkarenko on 6.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
